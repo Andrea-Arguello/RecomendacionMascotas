44d7b765098006dfc97cbe2d2af29f3a3b9d7b68
diazMafer <31084398+diazMafer@users.noreply.github.com>
2018-05-24 22:58:26 -0600

    link con networkx

--- Grafo1.py
+++ Grafo1.py
@@ -3,93 +3,88 @@
 # proyecto #2 - Estructura de datos
 #5/05/2018
 
-import networkx as nx
-G = nx.DiGraph()
 
-# agegar nodos
-G.add_node("Maria")
-G.add_node("Perro mediano")
-G.add_node("Erick")
-G.add_node("Ana")
-G.add_node("Si")
-G.add_node("No")
-G.add_node("Grande")
-G.add_node("Pequeno")
-G.add_node("Mediano")
-G.add_node("Activo")
-G.add_node("No activo")
-G.add_node("No tiene ninos")
-G.add_node("0-3")
-G.add_node("4-7")
-G.add_node("8-11")
-G.add_node("Alto P.")
-G.add_node("Bajo P.")
-G.add_node("Introvertido")
-G.add_node("Extrovertido")
-G.add_node("Tiene ninos", Animales="Gato, Perro mediano, Perro grande")
-G.add_node("Regular P.")
-G.add_node("Gato")
-G.add_node("Hamster")
+from neo4jrestclient.client import GraphDatabase
+from neo4jrestclient import client
+import sys
 
 
-print ("Nodos: ", G.nodes())
-
-# agregar aristas (relaciones)
-G.add_edge("Maria", "Regular P.")
-G.add_edge("Maria", "Si")
-G.add_edge("Maria", "Mediano")
-G.add_edge("Maria", "No tiene ninos", animales="Hamster, Perico, Loro")
-G.add_edge("Maria", "4-7")
-G.add_edge("Maria", "Extrovertido")
-G.add_edge("Maria", "No activo")
-G.add_edge("Erick", "Alto P.")
-G.add_edge("Erick", "No")
-G.add_edge("Erick", "Pequeno")
-G.add_edge("Erick", "No tiene ninos")
-G.add_edge("Erick", "4-7")
-G.add_edge("Erick", "Introvertido")
-G.add_edge("Erick", "Activo")
-G.add_edge("Ana", "Bajo P.")
-G.add_edge("Ana", "Si")
-G.add_edge("Ana", "Hamster")
-G.add_edge("Ana", "Mediano")
-G.add_edge("Ana", "8-11")
-G.add_edge("Ana", "Tiene ninos")
-G.add_edge("Ana", "Extrovertido")
-G.add_edge("Ana", "Activo")
-G.add_edge("Regular P.", "Perro mediano")
-G.add_edge("Si", "Perro mediano")
-G.add_edge("Grande", "Perro mediano")
-G.add_edge("Tiene ninos", "Perro mediano")
-G.add_edge("4-7", "Perro mediano")
-G.add_edge("Introvertido", "Perro mediano")
-G.add_edge("Activo", "Perro mediano")
-G.add_edge("Bajo P.", "Gato")
-G.add_edge("Si", "Gato")
-G.add_edge("Mediano", "Gato")
-G.add_edge("Tiene ninos", "Gato")
-G.add_edge("4-7", "Gato")
-G.add_edge("Extrovertido", "Gato")
-G.add_edge("No activo", "Gato")
-G.add_edge("Bajo P.", "Hamster")
-G.add_edge("Si", "Hamster")
-G.add_edge("Pequeno", "Hamster")
-G.add_edge("No tiene ninos", "Hamster")
-G.add_edge("8-11", "Hamster")
-G.add_edge("Introvertido", "Hamster")
-G.add_edge("No activo", "Hamster")
-
-print ("Aristas: ", G.edges())
-
-# Breath First Search
-
-print ("BFS:")
-print (list(nx.bfs_tree(G,"Maria")))
-#print (nx.bfs_successors(G,"Maria"))
+driver = GraphDatabase("http://localhost:7474", username="neo4j", password="mypassword")
 
 
-print ("DFS:")
-#print (list(nx.dfs_preorder_nodes(G,"Ana")))
+
+usuario = driver.labels.create("Usuario")
+animal = driver.labels.create("Animal")
+tiempo = driver.labels.create("Tiempo")
+espacio = driver.labels.create("Espacio")
+presupuesto = driver.labels.create("Presupuesto")
+personalidad = driver.labels.create("Personalidad")
+tipoMas = driver.labels.create("TipoM")
+ninos = driver.labels.create("Ninos")
+personalidad = driver.labels.create("Personalidad")
+alergia = driver.labels.create("Alergia")
+
+#-------- CREACION NODOS ---------
+#Yo diria que los unicos nodos que tenemos que hacer son los de los usuarios porque los otros ya los tendriamos en la base de datos metidos por default
+#porque no e como que fueramos a pedir info de animales o si?
+def add_usuario(nombre, telefono, correo, edad):
+    u1 = driver.nodes.create(Nombre=nombre, Telefono=telefono, Correo=corre, Edad=edad)
+    usuarios.add(u1)
+    return u1
+
+
+#------ RELACIONES -----
+def esMascota(animal, tipo):
+    q = 'MATCH (u:Animal), (s:TipoM) WHERE u.Nombre=\"'+ animal +'\" AND s.Tipo=\"'+ tipo +'\" RETURN u,s'
+    resultados = driver.query(q, returns=(client.Node, client.Node))
+    for i in resultados:
+        a = i[0]
+        t = i[1]
+        a.relationships.create("Es_una_mascota", t)
+
+def haTenido(usuario, animal):
+    q = 'MATCH (u:Usuario), (s:Animal) WHERE u.Nombre=\"'+ usuario +'\" AND s.Nombre=\"'+ animal +'\" RETURN u,s'
+    resultados = driver.query(q, returns=(client.Node, client.Node))
+    for i in resultados:
+        u = i[0]
+        a = i[1]
+        u.relationships.create("Ha_Tenido", a)
+
+def espacio(usuario, espacio):
+    q = 'MATCH (u:Usuario), (s:Espacio) WHERE u.Nombre=\"'+ usuario +'\" AND s.Tamano=\"'+ espacio +'\" RETURN u,s'
+    resultados = driver.query(q, returns=(client.Node, client.Node))
+    for i in resultados:
+        u = i[0]
+        e = i[1]
+        u.relationships.create("Espacio_disponible", e)
+
+def prefMas(usuario, tipo):
+    q = 'MATCH (u:Usuario), (s:TipoM) WHERE u.Nombre=\"'+ usuario +'\" AND s.Tipo=\"'+ tipo +'\" RETURN u,s'
+    resultados = driver.query(q, returns=(client.Node, client.Node))
+    for i in resultados:
+        u = i[0]
+        t = i[1]
+        u.relationships.create("Prefiere_una_mascota", t)
+
+def relacionAA(animal, alergia):
+    q = 'MATCH (u:Animal), (s:Alergia) WHERE u.Nombre=\"'+ animal +'\" AND s.Posee=\"'+ alergia +'\" RETURN u,s'
+    resultados = driver.query(q, returns=(client.Node, client.Node))
+    for i in resultados:
+        a = i[0]
+        al = i[1]
+        a.relationships.create("Genera_alergia", al)
+
+def relacionUA(usuario, alergia):
+    q = 'MATCH (u:Usuario), (s:Alergia) WHERE u.Nombre=\"'+ usuario +'\" AND s.Posee=\"'+ alergia +'\" RETURN u,s'
+    resultados = driver.query(q, returns=(client.Node, client.Node))
+    for i in resultados:
+        u = i[0]
+        al = i[1]
+        u.relationships.create("Tiene_alergia", al)
+        
+
+
+            
 
 
 
