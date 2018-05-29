27102585cf5649b5af1d1987d2c7fa99660e0b30
her17138 <her17138@uvg.edu.gt>
2018-05-29 10:14:07 -0600

    add moduo y Principal

---
+++ Principal.py
@@ -0,0 +1,95 @@
+# -*- coding: cp1252 -*-
+# Posible ejemplificacion de algortimo de recomendacion
+# Maria Fernanda Lopez, Ana Lucia Hernandez, Andrea Arguello
+# proyecto #2 - Estructura de datos
+#5/05/2018
+
+
+from hdt10 import *
+
+ciclo = 0
+
+
+while(ciclo==0):
+    
+    print ('\nQue desea hacer? \n>>1. Ingresar un doctor \n>>2. Ingresar un paciente \n>>3. Ingresar visita de un paciente a un doctor \n>>4. Consultar doctores por especialidad \n>>5. Ingresar relacion entre personas \n>>6. Buscar recomendacion del doctor para doctor con especialidad \n>>7. Buscar recomendacion de conocidos de doctor con especialidad \n>>8. Salir')
+
+    entrada= raw_input(">>")
+    print "\n**Usted ingreso: ", entrada,"**\n"
+
+
+
+    if(entrada=="1"):
+        name = raw_input("Ingrese el nombre del doctor: ")
+        tel = raw_input("Ingrese el numero de contacto: ")
+        col = raw_input("Ingrese el numero de colegiado: ")
+        esp = raw_input("Ingrese la especialidad: ")
+        add_Doctor(name,tel,col,esp)
+        print '**Doctor ingresado**\n'
+        ciclo = 0
+    elif(entrada=="2"):
+        name = raw_input("Ingrese el nombre del paciente: ")
+        tel = raw_input("Ingrese el numero de contacto: ")
+        add_Paciente(name,tel)
+        print '**Paciente ingresado**\n'
+        ciclo = 0
+    elif(entrada=="3"):
+        doctor = raw_input("Ingrese el nombre del doctor visitado: ")
+        paciente = raw_input("Ingrese el nombre del paciente: ")
+        fecha = raw_input("Ingrese la fecha de la visita: ")
+        medicina = raw_input("Ingrese la medicina recetada: ")
+        desde = raw_input("La tomara desde: ")
+        hasta = raw_input("La tomara hasta: ")
+        dosis = raw_input("Ingrese la dosis a tomar: ")
+        registrarVisita(paciente,doctor,fecha,medicina,desde,hasta,dosis)
+               
+        ciclo = 0
+    elif(entrada=="4"):
+        esp = raw_input("Ingrese la especialidad que desea buscar: ")
+        listaDocs = queryEsp(esp)
+        print("\nDOCTORES QUE TIENEN LA ESPECIALIDAD DE "+esp.upper())
+        for i in listaDocs:
+            print("--> "+i)
+        ciclo = 0
+    elif(entrada=="5"):
+        newciclo=0
+        while (newciclo==0):
+            opcion=raw_input("Son las personas:\n>>1. Doctor y Doctor\n>>2. Paciente y paciente\n>>")
+            if(opcion!="1" and opcion!="2" and opcion!="3"):
+                print "Ingrese una opcion valida"
+                newciclo=0
+            else:
+                if(opcion=="1"):
+                    persona1 = raw_input("Ingrese el primer doctor a relacionar: ")
+                    persona2 = raw_input("Ingrese el segundo doctor a relacionar: ")
+                    relacionDD(persona1,persona2)
+                    newciclo=1
+                    ciclo = 0
+                elif(opcion=="2"):
+                    persona1 = raw_input("Ingrese el primer paciente a relacionar: ")
+                    persona2 = raw_input("Ingrese el segundo paciente a relacionar: ")
+                    relacionPP(persona1,persona2)
+                    newciclo=1
+                    ciclo = 0
+    elif(entrada=="6"):
+        esp = raw_input("Ingrese la especialidad que desea buscar: ")
+        doc = raw_input("Ingrese el nombre del doctor que desea le haga la recomendacion: ")
+        recomendacionConocidosDoctor(esp, doc)
+        ciclo = 0
+    elif(entrada=="7"):
+        esp = raw_input("Ingrese la especialidad que desea buscar: ")
+        pac = raw_input("Ingrese el nombre del paciente que desea le haga la recomendacion: ")
+        recomendacionConocidosPaciente(pac, esp)
+        ciclo = 0
+    elif(entrada=="8"):
+        print "Feliz dia"
+        ciclo = 1
+    else:
+        print "Ingrese una opcion valida\n"
+        ciclo = 0
+
+    
+        
+        
+        
+
---
+++ modulo.py
@@ -0,0 +1,303 @@
+# -*- coding: cp1252 -*-
+# Posible ejemplificacion de algortimo de recomendacion
+# Maria Fernanda Lopez, Ana Lucia Hernandez, Andrea Arguello
+# proyecto #2 - Estructura de datos
+#5/05/2018
+
+
+from neo4jrestclient.client import GraphDatabase
+from neo4jrestclient import client
+import openpyxl
+import sys
+
+
+driver = GraphDatabase("http://localhost:7474", username="neo4j", password="mypassword")
+
+rangoPresu = ["Poco presupuesto", "Mediano presupuesto", "Alto presupuesto"]
+rangoEspacio = ["Pequeno", "Grande", "Moderado"]
+ninosPeques = ["Si", "No"]
+hrSemana = ["0-3", "4-7", "8-11"]
+personalidad = ["Extrovertida", "Introvertida"]
+tipoMas = ["Activa", "Inactiva"]
+
+
+usuario = driver.labels.create("Usuario")
+animal = driver.labels.create("Animal")
+tiempo = driver.labels.create("Tiempo")
+espacio = driver.labels.create("Espacio")
+presupuesto = driver.labels.create("Presupuesto")
+personalidad = driver.labels.create("Personalidad")
+tipoMas = driver.labels.create("TipoM")
+ninos = driver.labels.create("Ninos")
+personalidad = driver.labels.create("Personalidad")
+alergia = driver.labels.create("Alergia")
+
+
+#------------------------------------------------------------------------------
+
+#-------- CREACION NODOS POR DEFAULT EN EL GRAFO ---------
+
+#para agregar los nodos del presupuestos
+def add_presu():
+    for i in rangoPresu:
+        pres = driver.nodes.create(i)
+        presupuesto.add(pres)
+
+#agregar un Si o un No para hacer las relaciones de si tiene niños pequeños
+def add_ninos():
+    for i in ninosPeques:
+        nn = driver.nodes.crate(i)
+        ninos.add(nn)
+
+#Agregar nodos de rango de tiempo que le puede dedicar por semana
+def add_tiempo():
+    for i in hrSemana:
+        tt = driver.nodes.create(i)
+        tiempo.add(tt)
+
+#agregar los nodos de personalidad (extrovertido o introvertido) para la personalidad de usuario
+def add_personalidad():
+    for i in personalidad:
+        pp = driver.nodes.create(i)    #seria bueno tal vez ponerle otro nombre pero la mera verdad no se me ocurre
+        personalidad.add(pp)
+
+#agregar un Si o un No de si tiene alergia el usuario o si el animal genera  o no alergias
+def add_alergia():
+    for i in ninosPeques:
+        al = driver.nodes.create(i)
+        alergia.add(al)
+
+#agregar un nodo de activo o inactivo para el tipo de mascota
+def add_tipoM():
+    for i in tipoMas():
+        tipo = driver.nodes.create(i)
+        tipoMas.add(tipo)
+
+def add_preusuarios():
+    #aqui se tendria que leer el archivo de texto de la base de datos de analu + el de la db de google
+    archivo = open("users.txt", "r")  #esto es supositorio (el nombre)
+    contenido = archivo.readlines()
+    archivo.close()
+    for lineas in contenido:
+        users = lineas.split(", ")
+        ul = driver.nodes.create(Nombre=users[0], Tenido=user[1], Espacio=users[2], Ninos=users[3], Tiempo=users[4], Personalidad=users[5], Tipo=users[6], Presupuesto=users[7], Alergia=users[8])
+        usuario.add(ul)
+        prefMas(ul, users[7])
+        relacionUA(ul, users[2])
+        relacionPresupuesto(ul, users[1])
+        espacio(ul, users[3])
+        relacionNinos(ul, users[4])
+        tiempoUser(ul, users[5])
+        personalidadUsuario(ul, users[6])
+
+def add_animal():
+    archivo = open("animales.txt", "r")  #esto es supositorio (el nombre)
+    contenido = archivo.readlines()
+    archivo.close()
+    for lineas in contenido:
+        animales = lineas.split(", ")
+        an = driver.nodes.create(Nombre=animales[0], Presupuesto=animales[1], Alergia=animales[2], Espacio=animales[3], Ninos=animales[4], Tiempo=animales[5], Personalidad=animales[6], Tipo=animales[7])
+        animal.add(an)
+        esMascota(an, animales[7])
+        relacionAA(an, animales[2])
+        presupuestoAni(an, animales[1])
+        espacioAni(an, animales[3])
+        amigable(an, animales[4])
+        tiempoAni(an, animales[5])
+        personalidadAni(an, animales[6])
+
+        
+
+#-------------------------------------------------------------------------------------------
+#----- CREACION DE NODOS EN TIEMPO DE EJECUCION ---------------------
+    
+def add_usuario(nombre):
+    u1 = driver.nodes.create(Nombre=nombre)
+    usuarios.add(u1)
+    return u1
+
+
+
+#---------------------------------------------------------------------------------------
+
+
+#------ RELACIONES ANIMALES -----
+
+
+def esMascota(animal, tipo):
+    q = 'MATCH (u:Animal), (s:TipoM) WHERE u.Nombre=\"'+ animal +'\" AND s.Tipo=\"'+ tipo +'\" RETURN u,s'
+    resultados = driver.query(q, returns=(client.Node, client.Node))
+    for i in resultados:
+        a = i[0]
+        t = i[1]
+        a.relationships.create("Es_una_mascota", t)
+
+
+def relacionAA(animal, alergia):
+    q = 'MATCH (u:Animal), (s:Alergia) WHERE u.Nombre=\"'+ animal +'\" AND s.Alergia=\"'+ alergia +'\" RETURN u,s'
+    resultados = driver.query(q, returns=(client.Node, client.Node))
+    for i in resultados:
+        a = i[0]
+        al = i[1]
+        a.relationships.create("Genera_alergia", al)
+
+def presupuestoAni(animal, presupuesto):
+    q = 'MATCH (u:Animal), (s:Presupuesto) WHERE u.Nombre=\"'+ animal +'\" AND s.Presupuesto=\"'+ presupuesto +'\" RETURN u,s'
+    resultados = driver.query(q, returns=(client.Node, client.Node))
+    for i in resultados:
+        u = i[0]
+        pre = i[1]
+        a.relationships.create("Presupuesto_animal", pre)
+
+def espacioAni(animal, espacio):
+    q = 'MATCH (u:Animal), (s:Espacio) WHERE u.Nombre=\"'+ animal +'\" AND s.Espacio=\"'+ espacio +'\" RETURN u,s'
+    resultados = driver.query(q, returns=(client.Node, client.Node))
+    for i in resultados:
+        u = i[0]
+        e = i[1]
+        u.relationships.create("Necesita_espacio", e)
+
+def amigable(animal, ninos):
+    q = 'MATCH (u:Animal), (s:Ninos) WHERE u.Nombre=\"'+ animal +'\" AND s.Ninos=\"'+ ninos +'\" RETURN u,s'
+    resultados = driver.query(q, returns=(client.Node, client.Node))
+    for i in resultados:
+        u = i[0]
+        ni = i[1]
+        u.relationships.create("Amigable_ninos", ni)
+
+def tiempoAni(animal, tiempo):
+    q = 'MATCH (u:Animal), (s:Tiempo) WHERE u.Nombre=\"'+ animal +'\" AND s.Tiempo=\"'+ tiempo +'\" RETURN u,s'
+    resultados = driver.query(q, returns=(client.Node, client.Node))
+    for i in resultados:
+        u = i[0]
+        pre = i[1]
+        a.relationships.create("Tiempo_animal", pre)
+
+def personalidadAni(animal, personalidad):
+    q = 'MATCH (u:Animal), (s:Personalidad) WHERE u.Nombre=\"'+ animal +'\" AND s.Tipo=\"'+ personalidad +'\" RETURN u,s'
+    resultados = driver.query(q, returns=(client.Node, client.Node))
+    for i in resultados:
+        u = i[0]
+        t = i[1]
+        u.relationships.create("Personalidad_animal", t)
+    
+
+    
+#----------------------------------------------------------------------------------------------------
+
+
+#------ RELACIONES USUARIOS -----
+def haTenido(usuario, animal):
+    q = 'MATCH (u:Usuario), (s:Animal) WHERE u.Nombre=\"'+ usuario +'\" AND s.Nombre=\"'+ animal +'\" RETURN u,s'
+    resultados = driver.query(q, returns=(client.Node, client.Node))
+    for i in resultados:
+        u = i[0]
+        a = i[1]
+        u.relationships.create("Ha_Tenido", a)
+        
+#no cree los nodos de "Espacio" porque como esos estan por default pensaba que los metieramos de una en neo4j de una con los atributos con este nombre
+def espacio(usuario, espacio):
+    q = 'MATCH (u:Usuario), (s:Espacio) WHERE u.Nombre=\"'+ usuario +'\" AND s.Espacio=\"'+ espacio +'\" RETURN u,s'
+    resultados = driver.query(q, returns=(client.Node, client.Node))
+    for i in resultados:
+        u = i[0]
+        e = i[1]
+        u.relationships.create("Espacio_disponible", e)
+
+#no cree los nodos de "TipoM" porque como esos estan por default pensaba que los metieramos de una en neo4j de una con los atributos con este nombre
+def prefMas(usuario, tipo):
+    q = 'MATCH (u:Usuario), (s:TipoM) WHERE u.Nombre=\"'+ usuario +'\" AND s.Tipo=\"'+ tipo +'\" RETURN u,s'
+    resultados = driver.query(q, returns=(client.Node, client.Node))
+    for i in resultados:
+        u = i[0]
+        t = i[1]
+        u.relationships.create("Prefiere_una_mascota", t)
+
+#no cree los nodos de "Presupuesto" porque como esos estan por default pensaba que los metieramos de una en neo4j de una con los atributos con este nombre
+def relacionPresupuesto(usuario, presupuesto):
+    q = 'MATCH (u:Usuario), (s:Presupuesto) WHERE u.Nombre=\"'+ usuario +'\" AND s.Presupuesto=\"'+ presupuesto +'\" RETURN u,s'
+    resultados = driver.query(q, returns=(client.Node, client.Node))
+    for i in resultados:
+        u = i[0]
+        pre = i[1]
+        a.relationships.create("Presupuesto_usuario", pre)
+
+
+#no cree los nodos de "Alergia" porque como esos estan por default pensaba que los metieramos de una en neo4j de una con los atributos con este nombre
+def relacionUA(usuario, alergia):
+    q = 'MATCH (u:Usuario), (s:Alergia) WHERE u.Nombre=\"'+ usuario +'\" AND s.Alergia=\"'+ alergia +'\" RETURN u,s'
+    resultados = driver.query(q, returns=(client.Node, client.Node))
+    for i in resultados:
+        u = i[0]
+        al = i[1]
+        u.relationships.create("Tiene_alergia", al)
+
+#no cree los nodos de "Ninos" porque como esos estan por default pensaba que los metieramos de una en neo4j de una con los atributos con este nombre
+def relacionNinos(usuario, ninos):
+    q = 'MATCH (u:Usuario), (s:Ninos) WHERE u.Nombre=\"'+ usuario +'\" AND s.Ninos=\"'+ ninos +'\" RETURN u,s'
+    resultados = driver.query(q, returns=(client.Node, client.Node))
+    for i in resultados:
+        u = i[0]
+        ni = i[1]
+        u.relationships.create("Tiene_ninos", ni)
+
+def personalidadUsuario(usuario, personalidad):
+    q = 'MATCH (u:Usuario), (s:Personalidad) WHERE u.Nombre=\"'+ usuario +'\" AND s.Tipo=\"'+ personalidad +'\" RETURN u,s'
+    resultados = driver.query(q, returns=(client.Node, client.Node))
+    for i in resultados:
+        u = i[0]
+        t = i[1]
+        u.relationships.create("Personalidad", t)
+    
+
+#Relacion para realizar el algoritmo de recomendacion hibrido
+def relacionUU(usuario, usuario1):
+    q='MATCH (u:Usuario) WHERE u.Nombre=\"'+ usuario1 +'\" RETURN u'
+    doctores1 = driver.query(q,returns=(client.Node,str,client.Node))
+    for p in doctores1:
+        print("(%s)" % (p[0]["Nombre"]))
+    p1=p[0]
+
+    r='MATCH (u:Usuario) WHERE u.Nombre=\"'+ usuario +'\" RETURN u'
+    doctores = driver.query(r,returns=(client.Node,str,client.Node))
+    for r in doctores:
+        print("(%s)" % (r[0]["Nombre"]))
+    d1=r[0]
+    d1.relationships.create("CONOCE", p1)
+
+def tiempoUser(usuario, tiempo):
+    q = 'MATCH (u:Usuario), (s:Tiempo) WHERE u.Nombre=\"'+ usuario +'\" AND s.Tiempo=\"'+ tiempo +'\" RETURN u,s'
+    resultados = driver.query(q, returns=(client.Node, client.Node))
+    for i in resultados:
+        u = i[0]
+        pre = i[1]
+        a.relationships.create("Tiempo", pre)
+
+
+#parcialmente terminado
+def registrarInfo(usuario, ninos, tiempo, espacio, tipo, personalidad, alergia, presupuesto):
+    relacionNinos(usuarion, ninos)
+    relacionUA(usuario, alergia)
+
+
+#-------------------------------------------------------------------------------------------------------------
+
+
+# ------- QUERYS -----
+def getConocidosUser(nombreUser):
+    conocidosL=[]
+    q = 'MATCH (u:Usuario)-[r:CONOCE]->(m:Paciente) WHERE u.Nombre=\"'+ nombreUser +'\" RETURN u, type(r), m'
+    conocidos = driver.query(q, returns=(client.Node, str, client.Node))
+    if len(conocidos) ==0:
+        print("\n-----> La persona que ingreso no tiene conocidos o no existe :(")
+    else:
+        for r in conocidos:
+            conocidosL.append(r[2]["Nombre"])
+        return conocidosL
+    
+
+
+            
+
+
+
